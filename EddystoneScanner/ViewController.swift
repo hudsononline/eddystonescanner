9666613dafde8ad1a863ccfe293d72961d6de8cb
Qingyun Xiu <admin@qingyuns-macbook-pro.local>
2018-03-30 15:13:10 -0400

    Initial Commit

---
+++ EddystoneScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EddystoneScanner
+//
+//  Created by Qingyun Xiu on 3/30/18.
+//  Copyright © 2018 Qingyun Xiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
