6cb144ac9ee7e78ad9dd27727aa91f77710bdfa4
Qingyun Xiu <admin@Qingyuns-MacBook-Pro.local>
2018-04-01 08:40:51 -0400

    First commit

--- EddystoneScanner/ViewController.swift
+++ EddystoneScanner/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, BeaconScannerDelegate {
+    
+    var beaconScanner: BeaconScanner!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.beaconScanner = BeaconScanner()
+        self.beaconScanner!.delegate = self
+        self.beaconScanner!.startScanning()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func didFindBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+        NSLog("FIND: %@", beaconInfo.description)
+    }
+    func didLoseBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+        NSLog("LOST: %@", beaconInfo.description)
+    }
+    func didUpdateBeacon(beaconScanner: BeaconScanner, beaconInfo: BeaconInfo) {
+        NSLog("UPDATE: %@", beaconInfo.description)
+    }
+    func didObserveURLBeacon(beaconScanner: BeaconScanner, URL: NSURL, RSSI: Int) {
+        NSLog("URL SEEN: %@, RSSI: %d", URL, RSSI)
+    }
 
 }
 
